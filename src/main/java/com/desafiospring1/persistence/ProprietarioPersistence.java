934538bf2fde641d58764e1a95c830ada32d2a9d
rarosario <rafael.rosario@mercadolivre.com>
2021-10-08 10:53:16 -0300

    Incluindo JavaDoc.

--- src/main/java/com/desafiospring1/persistence/ProprietarioPersistence.java
+++ src/main/java/com/desafiospring1/persistence/ProprietarioPersistence.java
@@ -14,6 +14,13 @@ public class ProprietarioPersistence {
     private ProprietarioJson proprietarioJson = new ProprietarioJson();
     private static List<Proprietario> listaProprietario = new ArrayList<>();
 
+    /**
+     *
+     * @param proprietario esperado tipo Proprietario
+     * @return Proprietario em arquivo Json
+     * @throws IOException
+     * @author Grupo 5 - Rafael
+     */
     public Proprietario cadastra(Proprietario proprietario) throws IOException {
         proprietario.setId(proprietarioJson.listar().size() + 1L);
         List<Proprietario> novaListaProprietario = proprietarioJson.listar();
@@ -22,10 +29,21 @@ public class ProprietarioPersistence {
         return proprietario;
     }
 
+    /**
+     *
+     * @return List de proprietarios em arquivo Json
+     @author Grupo 5 - Rafael
+     */
     public List<Proprietario> listagem() {
         return proprietarioJson.listar();
     }
 
+    /**
+     *
+     * @param id é esperado um objeto do tipo id(Long) de um proprietario
+     * @return List de Proprietarios em arquivo Json através do parametro id
+     * @author Grupo 5 - Rafael
+     */
     public Proprietario buscaProprietarioPorId(Long id) {
         for (Proprietario proprietario : proprietarioJson.listar()) {
             if (proprietario.getId().equals(id)) {
@@ -35,6 +53,13 @@ public class ProprietarioPersistence {
         return null;
     }
 
+    /**
+     *
+     * @param id é esperado um objeto do tipo id(Long) de um proprietario
+     * @return List de proprietario atualizada após a remoção
+     * @throws IOException
+     * @author Grupo 5 - Rafael
+     */
     public List<Proprietario> deletaProprietario(Long id) throws IOException {
         listaProprietario = proprietarioJson.listar();
         for(int i=0; i<listaProprietario.size();i++){
@@ -46,6 +71,13 @@ public class ProprietarioPersistence {
         return proprietarioJson.listar();
     }
 
+    /**
+     *
+     * @param proprietario é esperado um objeto do tipo proprietario
+     * @return List de proprietarios atualizada após uma alteração
+     * @throws IOException
+     * @author Grupo 5 - Rafael
+     */
     public Proprietario atualizaProprietario(Proprietario proprietario) throws IOException {
         listaProprietario = proprietarioJson.listar();
         for (int i = 0; i < listaProprietario.size(); i++) {
